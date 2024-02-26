f5b2126013a5f2ac79f5c7650f364076d932b760
Elinor <39811460+GoldenSnake@users.noreply.github.com>
2024-02-26 15:07:27 +0300

    Начата усложнённая задача
    Добавлены кнопки Reset и -, добавлено текстовое поле UITextView с отображением истории изменений.

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,22 +8,43 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBOutlet weak var countValue: UILabel!
     
-    @IBOutlet weak var countButton: UIButton!
+    @IBOutlet weak var countValue: UILabel!
+    @IBOutlet weak var changeHistory: UITextView!
+   
+    @IBOutlet weak var buttonMinus: UIButton!
+    @IBOutlet weak var buttonReset: UIButton!
+    @IBOutlet weak var buttonPlus: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         countValue.text = "Значение счётчика: 0"
+        changeHistory.text = "История изменений: " + "\n"
     }
     
     private var count: Int = 0
     
-    @IBAction func buttonDidTap() {
+    @IBAction func buttonTouchPlus() {
         count += 1
         countValue.text = "Значение счётчика: \(count)"
+        changeHistory.text.append("Значение изменено на +1." + "\n")
     }
+    
+    @IBAction func buttonTouchMinus() {
+        if count > 0 {
+            count -= 1
+            countValue.text = "Значение счётчика: \(count)"
+            changeHistory.text.append("Значение изменено на -1." + "\n")
+        } else {
+            changeHistory.text.append("Попытка уменьшить значение счетчика ниже 0." + "\n")
+        }
+    }
+    
+    @IBAction func buttonTouchReset() {
+        count = 0
+        countValue.text = "Значение счётчика: \(count)"
+        changeHistory.text.append("Значение сброшено." + "\n")
+    }
+    
 }
 
