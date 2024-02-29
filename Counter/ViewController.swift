32d6f7bfa7f144a63c38ceb15a023b8463de2156
Elinor <39811460+GoldenSnake@users.noreply.github.com>
2024-02-29 17:30:06 +0300

    Переименованы UI элементы

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -17,23 +17,24 @@ extension Date {
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var countValue: UILabel!
-    @IBOutlet weak var changeHistory: UITextView!
-   
-    @IBOutlet weak var buttonMinus: UIButton!
-    @IBOutlet weak var buttonReset: UIButton!
-    @IBOutlet weak var buttonPlus: UIButton!
+    
+    @IBOutlet weak var countLabel: UILabel!
+    @IBOutlet weak var historyTextView: UITextView!
+    
+    @IBOutlet weak var minusButton: UIButton!
+    @IBOutlet weak var plusButton: UIButton!
+    @IBOutlet weak var resetButton: UIButton!
     
     private func roundCornerButtons(){
-        buttonMinus.layer.cornerRadius = 15
-        buttonPlus.layer.cornerRadius = 15
-        buttonReset.layer.cornerRadius = 15
+        minusButton.layer.cornerRadius = 15
+        plusButton.layer.cornerRadius = 15
+        resetButton.layer.cornerRadius = 15
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        countValue.text = "Значение счётчика: 0"
-        changeHistory.text = "История изменений: " + "\n"
+        countLabel.text = "Значение счётчика: 0"
+        historyTextView.text = "История изменений: " + "\n"
         roundCornerButtons()
     }
     
@@ -41,24 +42,24 @@ class ViewController: UIViewController {
     
     @IBAction func buttonTouchPlus() {
         count += 1
-        countValue.text = "Значение счётчика: \(count)"
-        changeHistory.text.append("[\(Date().dateInFormat)]: значение изменено на +1." + "\n")
+        countLabel.text = "Значение счётчика: \(count)"
+        historyTextView.text.append("[\(Date().dateInFormat)]: значение изменено на +1." + "\n")
     }
     
     @IBAction func buttonTouchMinus() {
         if count > 0 {
             count -= 1
-            countValue.text = "Значение счётчика: \(count)"
-            changeHistory.text.append("[\(Date().dateInFormat)]: значение изменено на -1." + "\n")
+            countLabel.text = "Значение счётчика: \(count)"
+            historyTextView.text.append("[\(Date().dateInFormat)]: значение изменено на -1." + "\n")
         } else {
-            changeHistory.text.append("[\(Date().dateInFormat)]: попытка уменьшить значение счетчика ниже 0." + "\n")
+            historyTextView.text.append("[\(Date().dateInFormat)]: попытка уменьшить значение счетчика ниже 0." + "\n")
         }
     }
     
     @IBAction func buttonTouchReset() {
         count = 0
-        countValue.text = "Значение счётчика: \(count)"
-        changeHistory.text.append("[\(Date().dateInFormat)]: значение сброшено." + "\n")
+        countLabel.text = "Значение счётчика: \(count)"
+        historyTextView.text.append("[\(Date().dateInFormat)]: значение сброшено." + "\n")
     }
     
 }
